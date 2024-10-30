0de91556a063e0fc2aa54775a22455affe0d8eff
soh003 <149380282+soh003@users.noreply.github.com>
2024-10-30 08:42:51 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            intro:'Hello World',
+        }
+    }, 
+    methods: {
+        myMethod(){
+
+        },
+    },
+    computed:{
+        myComputed(){
+            return''
+        },
+    }
+
+
+})
